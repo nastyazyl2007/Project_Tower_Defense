3f333f0e865635d207f405f24f0bd3c8a7fe4019
nastyazyl2007 <nastyazyl2007@gmail.com>
2026-02-05 17:13:51 +0300

    1

---
+++ Project/Assets/Game_inside_assets/Scripts/Move_from_point_to_point.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using System.Collections;
+
+public class Move_from_point_to_point : MonoBehaviour
+{
+    [SerializeField] public float speed;
+    [SerializeField] public Transform[] point;
+    int current_waypoint_index = 0;
+
+    void Update()
+    {
+        if (current_waypoint_index < point.Length) {
+            transform.position = Vector3.MoveTowards(transform.position, point[current_waypoint_index].position,Time.deltaTime * speed);
+            if (Vector3.Distance(transform.position, point[current_waypoint_index].position) < 0.5f) current_waypoint_index++;
+        }
+    }
+}
---
+++ Project/Assets/Game_inside_assets/Scripts/Wave_spawn.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Collections;
+using UnityEngine;
+
+
+//Ввести сюда списки для обозначения сколько и каких врагов и где должны появиться на карте
+public class Wave_spawn : MonoBehaviour
+{
+    [SerializeField] public int enemy_number;      
+    [SerializeField] public int wave_number;      
+    [SerializeField] public GameObject enemy_type;
+    [SerializeField] public float spawn_enemy_interval;     
+    [SerializeField] public float time_between_waves;       
+    [SerializeField] public Transform spawn_point;
+    [SerializeField] public Transform[] way_point;
+    [SerializeField] public float time_before_first_wave;  
+
+    private int enemies_spawned_in_current_wave = 0;
+    private int current_wave_number = 0;
+    private bool is_spawning_wave = false;
+
+    void Start()
+    {
+        Invoke("StartNextWave", time_before_first_wave);
+    }
+
+    void StartNextWave()
+    {
+        if (current_wave_number >= wave_number)
+        {
+            Debug.Log("Все волны завершены!");
+            return;
+        }
+
+        current_wave_number++;
+        enemies_spawned_in_current_wave = 0;  
+        is_spawning_wave = true;
+
+        Debug.Log($"Начинается волна {current_wave_number}/{wave_number}");
+
+        InvokeRepeating("SpawnEnemyInCurrentWave", 0f, spawn_enemy_interval);
+    }
+
+    void SpawnEnemyInCurrentWave()
+    {
+        if (enemies_spawned_in_current_wave >= enemy_number)
+        {
+            CancelInvoke("SpawnEnemyInCurrentWave");
+            is_spawning_wave = false;
+            if (current_wave_number < wave_number)
+            {
+                Invoke("StartNextWave", time_between_waves);
+            }
+            return;
+        }
+
+        enemies_spawned_in_current_wave++;
+        GameObject enemy =Instantiate(enemy_type, spawn_point.position, Quaternion.identity);
+        enemy.GetComponent<Move_from_point_to_point>().point = way_point;
+
+        Debug.Log($"Волна {current_wave_number}: создан враг {enemies_spawned_in_current_wave}/{enemy_number}");
+    }
+
+    public void CompleteCurrentWave()
+    {
+        CancelInvoke("SpawnEnemyInCurrentWave");
+        is_spawning_wave = false;
+
+        if (current_wave_number < wave_number)
+        {
+            Invoke("StartNextWave", time_between_waves);
+        }
+    }
+}
