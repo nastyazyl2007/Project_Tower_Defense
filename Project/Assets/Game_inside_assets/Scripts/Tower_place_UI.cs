66f000efe37e50c1149d89ef2e66eb2d44fe0777
nastyazyl2007 <nastyazyl2007@gmail.com>
2026-02-07 16:25:55 +0300

    4

---
+++ Project/Assets/Game_inside_assets/Scripts/Tower_place_UI.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Tower_place_UI : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
